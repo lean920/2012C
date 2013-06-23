bc2ae3a0f1b80f547a166f94013cd3ece974227b
lijia <linjiajialin@126.com>
2013-06-23 16:22:18 +0800

    aaa

---
+++ src/Point.cpp
@@ -0,0 +1,18 @@
+/*
+ * Point.cpp
+ *
+ *  Created on: 2013-6-23
+ *      Author: Lean
+ */
+
+#include "Point.h"
+
+Point::Point() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Point::~Point() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Point.h
@@ -0,0 +1,17 @@
+/*
+ * Point.h
+ *
+ *  Created on: 2013-6-23
+ *      Author: Lean
+ */
+
+#ifndef POINT_H_
+#define POINT_H_
+
+class Point {
+public:
+	Point();
+	virtual ~Point();
+};
+
+#endif /* POINT_H_ */
